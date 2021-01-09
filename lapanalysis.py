0158f4c4072c1faf3224f8d2c8553b1109c6e3c5
k-uranium <uyama.kaname@gmail.com>
2021-01-09 15:27:29 +0900

    modify code

--- lapanalysis.py
+++ lapanalysis.py
@@ -4,8 +4,8 @@ import os
 import re
 from utils import getRaceDetail, getRaceName, getAveragePaceInfo, getTitle, getLink, analysis
 
-with open('./レース分析.txt', 'r') as f:
-    link_list = f.read().split('\n')
+with open('./raceanalysis.txt', 'r') as f:
+    link_list = f.read().splitlines()
 link = link_list.pop(0)
 if 'https://race.netkeiba.com/race/shutuba.html?race_id=' not in link:
     print('不正なURLです。' + link)
@@ -21,6 +21,9 @@ split_detail = re.split('[年月日]', detail)
 directoryname = './' + split_detail[0] + split_detail[1].zfill(2) + split_detail[2].zfill(
     2) + split_detail[3].replace(' ', '') + getRaceName(race_data.find(class_='RaceName'))[1]
 os.makedirs(directoryname, exist_ok=True)
+with open(os.path.join(directoryname, 'raceanalysis'), 'w') as f:
+    f.write(link + '\n')
+    f.write('\n'.join(link_list))
 race_data01_class = str(race_data.find_all(class_='RaceData01'))
 m_position = race_data01_class.find('m')
 distance = int(race_data01_class[m_position-4:m_position])
