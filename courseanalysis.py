963f91c497bd2c88e945b7cc1d010e526488b471
k-uranium <uyama.kaname@gmail.com>
2021-01-16 10:39:37 +0900

    modify code

--- courseanalysis.py
+++ courseanalysis.py
@@ -1,7 +1,10 @@
 from utils import getAveragePaceInfo
 
-with open('./courseanalysis.txt', 'r') as f:
+textfilename = 'courseanalysis.txt'
+with open('./' + textfilename, 'r') as f:
     link_list = f.read().split('\n')
 distance = int(link_list.pop(0))
 average_pace_info = getAveragePaceInfo('./', link_list, distance)
 print(average_pace_info)
+with open('./過去レース/' + textfilename, 'w') as f:
+    f.write('\n'.join(link_list))
--- lapanalysis.py
+++ lapanalysis.py
@@ -4,7 +4,8 @@ import os
 import re
 from utils import getRaceDetail, getRaceName, getAveragePaceInfo, getTitle, getLink, analysis
 
-with open('./raceanalysis.txt', 'r') as f:
+textfilename = 'raceanalysis.txt'
+with open('./' + textfilename, 'r') as f:
     link_list = f.read().splitlines()
 link = link_list.pop(0)
 if 'https://race.netkeiba.com/race/shutuba.html?race_id=' not in link:
@@ -21,7 +22,7 @@ split_detail = re.split('[年月日]', detail)
 directoryname = './' + split_detail[0] + split_detail[1].zfill(2) + split_detail[2].zfill(
     2) + split_detail[3].replace(' ', '') + getRaceName(race_data.find(class_='RaceName'))[1]
 os.makedirs(directoryname, exist_ok=True)
-with open(os.path.join(directoryname, 'raceanalysis.txt'), 'w') as f:
+with open(os.path.join(directoryname, textfilename), 'w') as f:
     f.write(link + '\n')
     f.write('\n'.join(link_list))
 race_data01_class = str(race_data.find_all(class_='RaceData01'))
